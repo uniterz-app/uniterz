8829babbde0756da9c0e3b0302ef55c7f8217553
Rikuto Kamiya <rikutokamiya@RikutonoMacBook-Pro.local>
2026-03-17 01:51:36 +0900

    restore stash work and fix bracket/profile

---
+++ app/web/(with-nav)/playoff-bracket/page.tsx
@@ -0,0 +1,9 @@
+import PlayoffBracketPredict from "@/app/component/predict/PlayoffBracketPredict";
+
+export default function Page() {
+  return (
+    <div className="max-w-6xl mx-auto p-8">
+      <PlayoffBracketPredict />
+    </div>
+  );
+}
